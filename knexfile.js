ce62a18bf4de72dc2a6f6cd3b82e37194c3854ab
Tim Davis <thebearingedge@gmail.com>
2016-07-12 18:00:09 -0700

    update knexfile to es6

--- knexfile.js
+++ knexfile.js
@@ -1,21 +1,16 @@
-// Update with your config settings.
 
-module.exports = {
-
-  development: {
-    client: 'postgresql',
-    connection: {
-      database: 'knex-js-workflows',
-      user:     'knex-js-workflows',
-      password: 'knex-js-workflows'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+export const development = {
+  client: 'postgresql',
+  connection: {
+    database: 'knex-js-workflows',
+    user:     'knex-js-workflows',
+    password: 'knex-js-workflows'
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
   }
-
-};
+}
